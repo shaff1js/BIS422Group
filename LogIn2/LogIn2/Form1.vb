d48b7eaabb1fe3a3292dcb9d83e13c1be7dc0dab
Jonathan Shaffer <shaff1js@cmich.edu>
2017-03-27 22:06:43 -0400

    need to add extension to vs.

--- LogIn2/LogIn2/Form1.vb
+++ LogIn2/LogIn2/Form1.vb
@@ -50,9 +50,24 @@ Public Class Form1
         Me.Close()
     End Sub
 
+    Private DB As New DBAccessClass
     Private Sub LogInButton_Click(sender As Object, e As EventArgs) Handles LogInButton.Click
-        'log in to the realty portal
 
+
+        'log in to the realty portal
+        DB.ExecuteQuery("SELECT EmployeeID, Password FROM HSHEmployee")
+
+        If DB.Exception <> String.Empty Then
+            MessageBox.Show(DB.Exception)
+            Exit Sub
+        End If
+
+        If UserIDTextBox.Text = "" And PasswordMaskedTextBox.Text = "" Then
+            MessageBox.Show("Success")
+        Else
+            MessageBox.Show("Fail")
+
+        End If
 
     End Sub
 End Class
