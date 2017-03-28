7aab8bb32044e190205e53d5400b3ddb971edcbd
Jonathan Shaffer <shaff1js@cmich.edu>
2017-03-27 21:15:43 -0400

    Added Beginning of VB shell.  Page 1 from course materials

---
+++ LogIn2/LogIn2/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LogInButton = New System.Windows.Forms.Button()
+        Me.CloseButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.UserIDTextBox = New System.Windows.Forms.TextBox()
+        Me.PasswordMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.SuspendLayout()
+        '
+        'LogInButton
+        '
+        Me.LogInButton.Location = New System.Drawing.Point(141, 222)
+        Me.LogInButton.Name = "LogInButton"
+        Me.LogInButton.Size = New System.Drawing.Size(75, 23)
+        Me.LogInButton.TabIndex = 0
+        Me.LogInButton.Text = "Log In"
+        Me.LogInButton.UseVisualStyleBackColor = True
+        '
+        'CloseButton
+        '
+        Me.CloseButton.Location = New System.Drawing.Point(325, 222)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(75, 23)
+        Me.CloseButton.TabIndex = 1
+        Me.CloseButton.Text = "Close"
+        Me.CloseButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(138, 98)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "User ID:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(138, 149)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password:"
+        '
+        'UserIDTextBox
+        '
+        Me.UserIDTextBox.Location = New System.Drawing.Point(300, 98)
+        Me.UserIDTextBox.Name = "UserIDTextBox"
+        Me.UserIDTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.UserIDTextBox.TabIndex = 4
+        '
+        'PasswordMaskedTextBox
+        '
+        Me.PasswordMaskedTextBox.Location = New System.Drawing.Point(300, 149)
+        Me.PasswordMaskedTextBox.Name = "PasswordMaskedTextBox"
+        Me.PasswordMaskedTextBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.PasswordMaskedTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.PasswordMaskedTextBox.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(532, 306)
+        Me.Controls.Add(Me.PasswordMaskedTextBox)
+        Me.Controls.Add(Me.UserIDTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CloseButton)
+        Me.Controls.Add(Me.LogInButton)
+        Me.Name = "Form1"
+        Me.Text = "Log In"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LogInButton As Button
+    Friend WithEvents CloseButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents UserIDTextBox As TextBox
+    Friend WithEvents PasswordMaskedTextBox As MaskedTextBox
+End Class
