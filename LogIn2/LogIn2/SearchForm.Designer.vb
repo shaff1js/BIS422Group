f3cb58261a1bb76bf3dff546bc015a4bbfb3c6b7
shaff1js <shaff1js@cmich.edu>
2017-04-05 00:34:37 -0400

    finished the search and purchase functions

--- LogIn2/LogIn2/SearchForm.Designer.vb
+++ LogIn2/LogIn2/SearchForm.Designer.vb
@@ -25,23 +25,39 @@ Partial Class SearchForm
         Me.BackButton = New System.Windows.Forms.Button()
         Me.HSHPropertyDataGridView = New System.Windows.Forms.DataGridView()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.NumberOfBedroomsComboBox = New System.Windows.Forms.ComboBox()
+        Me.askingPriceMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.SquareFeetMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SearchButton = New System.Windows.Forms.Button()
         Me.NumberOfBathroomsComboBox = New System.Windows.Forms.ComboBox()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.SearchButton = New System.Windows.Forms.Button()
-        Me.ClearButton = New System.Windows.Forms.Button()
-        Me.SquareFeetMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
-        Me.askingPriceMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.NumberOfBedroomsComboBox = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ClearPurchaseButton = New System.Windows.Forms.Button()
+        Me.PurchaseButton = New System.Windows.Forms.Button()
+        Me.DateSoldMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.SalePriceMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.RealEstateAgentIDMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.CustomerIDMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.PropertyIDTextBox = New System.Windows.Forms.TextBox()
+        Me.SaleIDTextBox = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
         CType(Me.HSHPropertyDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
         'BackButton
         '
-        Me.BackButton.Location = New System.Drawing.Point(814, 554)
+        Me.BackButton.Location = New System.Drawing.Point(779, 548)
         Me.BackButton.Name = "BackButton"
         Me.BackButton.Size = New System.Drawing.Size(75, 23)
         Me.BackButton.TabIndex = 0
@@ -51,7 +67,7 @@ Partial Class SearchForm
         'HSHPropertyDataGridView
         '
         Me.HSHPropertyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.HSHPropertyDataGridView.Location = New System.Drawing.Point(28, 235)
+        Me.HSHPropertyDataGridView.Location = New System.Drawing.Point(28, 233)
         Me.HSHPropertyDataGridView.Name = "HSHPropertyDataGridView"
         Me.HSHPropertyDataGridView.Size = New System.Drawing.Size(861, 150)
         Me.HSHPropertyDataGridView.TabIndex = 1
@@ -68,54 +84,52 @@ Partial Class SearchForm
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Location = New System.Drawing.Point(215, 31)
+        Me.GroupBox1.Location = New System.Drawing.Point(163, 31)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(484, 182)
+        Me.GroupBox1.Size = New System.Drawing.Size(573, 196)
         Me.GroupBox1.TabIndex = 2
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Search Properties"
         '
-        'Label1
+        'askingPriceMaskedTextBox
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(19, 37)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(109, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Number of Bedrooms:"
+        Me.askingPriceMaskedTextBox.Location = New System.Drawing.Point(214, 146)
+        Me.askingPriceMaskedTextBox.Mask = "0000000000"
+        Me.askingPriceMaskedTextBox.Name = "askingPriceMaskedTextBox"
+        Me.askingPriceMaskedTextBox.Size = New System.Drawing.Size(121, 20)
+        Me.askingPriceMaskedTextBox.TabIndex = 5
         '
-        'Label2
+        'SquareFeetMaskedTextBox
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(19, 73)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(112, 13)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "Number of Bathrooms:"
+        Me.SquareFeetMaskedTextBox.Location = New System.Drawing.Point(214, 110)
+        Me.SquareFeetMaskedTextBox.Mask = "000000000"
+        Me.SquareFeetMaskedTextBox.Name = "SquareFeetMaskedTextBox"
+        Me.SquareFeetMaskedTextBox.Size = New System.Drawing.Size(121, 20)
+        Me.SquareFeetMaskedTextBox.TabIndex = 4
         '
-        'Label3
+        'ClearButton
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(19, 109)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(68, 13)
-        Me.Label3.TabIndex = 0
-        Me.Label3.Text = "Square Feet:"
+        Me.ClearButton.Location = New System.Drawing.Point(411, 113)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(75, 39)
+        Me.ClearButton.TabIndex = 3
+        Me.ClearButton.Text = "Clear Search"
+        Me.ClearButton.UseVisualStyleBackColor = True
         '
-        'NumberOfBedroomsComboBox
+        'SearchButton
         '
-        Me.NumberOfBedroomsComboBox.FormattingEnabled = True
-        Me.NumberOfBedroomsComboBox.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"})
-        Me.NumberOfBedroomsComboBox.Location = New System.Drawing.Point(157, 34)
-        Me.NumberOfBedroomsComboBox.Name = "NumberOfBedroomsComboBox"
-        Me.NumberOfBedroomsComboBox.Size = New System.Drawing.Size(121, 21)
-        Me.NumberOfBedroomsComboBox.TabIndex = 1
+        Me.SearchButton.Location = New System.Drawing.Point(411, 48)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(75, 39)
+        Me.SearchButton.TabIndex = 2
+        Me.SearchButton.Text = "Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
         '
         'NumberOfBathroomsComboBox
         '
         Me.NumberOfBathroomsComboBox.FormattingEnabled = True
         Me.NumberOfBathroomsComboBox.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"})
-        Me.NumberOfBathroomsComboBox.Location = New System.Drawing.Point(157, 70)
+        Me.NumberOfBathroomsComboBox.Location = New System.Drawing.Point(214, 71)
         Me.NumberOfBathroomsComboBox.Name = "NumberOfBathroomsComboBox"
         Me.NumberOfBathroomsComboBox.Size = New System.Drawing.Size(121, 21)
         Me.NumberOfBathroomsComboBox.TabIndex = 1
@@ -123,51 +137,199 @@ Partial Class SearchForm
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(19, 145)
+        Me.Label4.Location = New System.Drawing.Point(76, 149)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(69, 13)
         Me.Label4.TabIndex = 0
         Me.Label4.Text = "Asking Price:"
         '
-        'SearchButton
+        'NumberOfBedroomsComboBox
         '
-        Me.SearchButton.Location = New System.Drawing.Point(340, 51)
-        Me.SearchButton.Name = "SearchButton"
-        Me.SearchButton.Size = New System.Drawing.Size(75, 39)
-        Me.SearchButton.TabIndex = 2
-        Me.SearchButton.Text = "Search"
-        Me.SearchButton.UseVisualStyleBackColor = True
+        Me.NumberOfBedroomsComboBox.FormattingEnabled = True
+        Me.NumberOfBedroomsComboBox.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"})
+        Me.NumberOfBedroomsComboBox.Location = New System.Drawing.Point(214, 35)
+        Me.NumberOfBedroomsComboBox.Name = "NumberOfBedroomsComboBox"
+        Me.NumberOfBedroomsComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.NumberOfBedroomsComboBox.TabIndex = 1
         '
-        'ClearButton
+        'Label3
         '
-        Me.ClearButton.Location = New System.Drawing.Point(340, 103)
-        Me.ClearButton.Name = "ClearButton"
-        Me.ClearButton.Size = New System.Drawing.Size(75, 39)
-        Me.ClearButton.TabIndex = 3
-        Me.ClearButton.Text = "Clear Search"
-        Me.ClearButton.UseVisualStyleBackColor = True
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(76, 113)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Square Feet:"
         '
-        'SquareFeetMaskedTextBox
+        'Label2
         '
-        Me.SquareFeetMaskedTextBox.Location = New System.Drawing.Point(157, 105)
-        Me.SquareFeetMaskedTextBox.Mask = "000000000"
-        Me.SquareFeetMaskedTextBox.Name = "SquareFeetMaskedTextBox"
-        Me.SquareFeetMaskedTextBox.Size = New System.Drawing.Size(121, 20)
-        Me.SquareFeetMaskedTextBox.TabIndex = 4
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(76, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(112, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Number of Bathrooms:"
         '
-        'askingPriceMaskedTextBox
+        'Label1
         '
-        Me.askingPriceMaskedTextBox.Location = New System.Drawing.Point(157, 142)
-        Me.askingPriceMaskedTextBox.Mask = "0000000000"
-        Me.askingPriceMaskedTextBox.Name = "askingPriceMaskedTextBox"
-        Me.askingPriceMaskedTextBox.Size = New System.Drawing.Size(121, 20)
-        Me.askingPriceMaskedTextBox.TabIndex = 5
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(76, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Number of Bedrooms:"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.ClearPurchaseButton)
+        Me.GroupBox3.Controls.Add(Me.PurchaseButton)
+        Me.GroupBox3.Controls.Add(Me.DateSoldMaskedTextBox)
+        Me.GroupBox3.Controls.Add(Me.SalePriceMaskedTextBox)
+        Me.GroupBox3.Controls.Add(Me.RealEstateAgentIDMaskedTextBox)
+        Me.GroupBox3.Controls.Add(Me.CustomerIDMaskedTextBox)
+        Me.GroupBox3.Controls.Add(Me.PropertyIDTextBox)
+        Me.GroupBox3.Controls.Add(Me.SaleIDTextBox)
+        Me.GroupBox3.Controls.Add(Me.Label10)
+        Me.GroupBox3.Controls.Add(Me.Label9)
+        Me.GroupBox3.Controls.Add(Me.Label8)
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Location = New System.Drawing.Point(163, 404)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(573, 184)
+        Me.GroupBox3.TabIndex = 3
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Sales Detail"
+        '
+        'ClearPurchaseButton
+        '
+        Me.ClearPurchaseButton.Location = New System.Drawing.Point(424, 90)
+        Me.ClearPurchaseButton.Name = "ClearPurchaseButton"
+        Me.ClearPurchaseButton.Size = New System.Drawing.Size(75, 23)
+        Me.ClearPurchaseButton.TabIndex = 17
+        Me.ClearPurchaseButton.Text = "Clear"
+        Me.ClearPurchaseButton.UseVisualStyleBackColor = True
+        '
+        'PurchaseButton
+        '
+        Me.PurchaseButton.Location = New System.Drawing.Point(424, 41)
+        Me.PurchaseButton.Name = "PurchaseButton"
+        Me.PurchaseButton.Size = New System.Drawing.Size(75, 23)
+        Me.PurchaseButton.TabIndex = 16
+        Me.PurchaseButton.Text = "Purchase"
+        Me.PurchaseButton.UseVisualStyleBackColor = True
+        '
+        'DateSoldMaskedTextBox
+        '
+        Me.DateSoldMaskedTextBox.Location = New System.Drawing.Point(214, 144)
+        Me.DateSoldMaskedTextBox.Mask = "0000/00/00"
+        Me.DateSoldMaskedTextBox.Name = "DateSoldMaskedTextBox"
+        Me.DateSoldMaskedTextBox.ReadOnly = True
+        Me.DateSoldMaskedTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.DateSoldMaskedTextBox.TabIndex = 15
+        Me.DateSoldMaskedTextBox.ValidatingType = GetType(Date)
+        '
+        'SalePriceMaskedTextBox
+        '
+        Me.SalePriceMaskedTextBox.Location = New System.Drawing.Point(214, 115)
+        Me.SalePriceMaskedTextBox.Mask = "0000000000"
+        Me.SalePriceMaskedTextBox.Name = "SalePriceMaskedTextBox"
+        Me.SalePriceMaskedTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.SalePriceMaskedTextBox.TabIndex = 14
+        '
+        'RealEstateAgentIDMaskedTextBox
+        '
+        Me.RealEstateAgentIDMaskedTextBox.Location = New System.Drawing.Point(214, 90)
+        Me.RealEstateAgentIDMaskedTextBox.Mask = "000000"
+        Me.RealEstateAgentIDMaskedTextBox.Name = "RealEstateAgentIDMaskedTextBox"
+        Me.RealEstateAgentIDMaskedTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.RealEstateAgentIDMaskedTextBox.TabIndex = 13
+        '
+        'CustomerIDMaskedTextBox
+        '
+        Me.CustomerIDMaskedTextBox.Location = New System.Drawing.Point(214, 66)
+        Me.CustomerIDMaskedTextBox.Mask = "000000"
+        Me.CustomerIDMaskedTextBox.Name = "CustomerIDMaskedTextBox"
+        Me.CustomerIDMaskedTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.CustomerIDMaskedTextBox.TabIndex = 12
+        '
+        'PropertyIDTextBox
+        '
+        Me.PropertyIDTextBox.Location = New System.Drawing.Point(214, 41)
+        Me.PropertyIDTextBox.Name = "PropertyIDTextBox"
+        Me.PropertyIDTextBox.ReadOnly = True
+        Me.PropertyIDTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.PropertyIDTextBox.TabIndex = 8
+        '
+        'SaleIDTextBox
+        '
+        Me.SaleIDTextBox.Location = New System.Drawing.Point(214, 16)
+        Me.SaleIDTextBox.Name = "SaleIDTextBox"
+        Me.SaleIDTextBox.ReadOnly = True
+        Me.SaleIDTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.SaleIDTextBox.TabIndex = 6
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(76, 23)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(45, 13)
+        Me.Label10.TabIndex = 5
+        Me.Label10.Text = "Sale ID:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(76, 123)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(58, 13)
+        Me.Label9.TabIndex = 4
+        Me.Label9.Text = "Sale Price:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(76, 98)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(110, 13)
+        Me.Label8.TabIndex = 3
+        Me.Label8.Text = "Real Estate Agent ID:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(76, 73)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(68, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Customer ID:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(76, 48)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(63, 13)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Property ID:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(76, 148)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Date Sold:"
         '
         'SearchForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(918, 600)
+        Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.HSHPropertyDataGridView)
         Me.Controls.Add(Me.BackButton)
@@ -176,6 +338,8 @@ Partial Class SearchForm
         CType(Me.HSHPropertyDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -193,4 +357,19 @@ Partial Class SearchForm
     Friend WithEvents Label1 As Label
     Friend WithEvents askingPriceMaskedTextBox As MaskedTextBox
     Friend WithEvents SquareFeetMaskedTextBox As MaskedTextBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents ClearPurchaseButton As Button
+    Friend WithEvents PurchaseButton As Button
+    Friend WithEvents DateSoldMaskedTextBox As MaskedTextBox
+    Friend WithEvents SalePriceMaskedTextBox As MaskedTextBox
+    Friend WithEvents RealEstateAgentIDMaskedTextBox As MaskedTextBox
+    Friend WithEvents CustomerIDMaskedTextBox As MaskedTextBox
+    Friend WithEvents PropertyIDTextBox As TextBox
+    Friend WithEvents SaleIDTextBox As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
 End Class
--- LogIn2/LogIn2/SearchForm.vb
+++ LogIn2/LogIn2/SearchForm.vb
@@ -9,13 +9,16 @@
 
 
     Private Sub Retrieve_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        DB.ExecuteQuery("SELECT PropertyID, PropertyStreetAddress, ZipCode, AskingPrice, NumberOfBedrooms, NumberOfBathrooms, TotalSquareFeet, YearBuilt FROM HSHProperty ORDER BY PropertyID")
+        DB.ExecuteQuery("SELECT PropertyID, PropertyStreetAddress, ZipCode, AskingPrice, NumberOfBedrooms, NumberOfBathrooms, TotalSquareFeet, YearBuilt FROM HSHProperty WHERE Sold = 'n' ORDER BY PropertyID")
         If DB.Exception <> String.Empty Then
             MessageBox.Show(DB.Exception)
             Exit Sub
         End If
 
         HSHPropertyDataGridView.DataSource = DB.DBDataTable
+
+        PurchaseButton.Enabled = False
+        ClearPurchaseButton.Enabled = False
 
     End Sub
 
@@ -24,12 +27,12 @@
         DB.AddParam("@NumberOfBathrooms", NumberOfBathroomsComboBox.Text & "%")
         DB.AddParam("@TotalSquareFeet", SquareFeetMaskedTextBox.Text & "%")
         If askingPriceMaskedTextBox.text.length = 0 Then
-             DB.ExecuteQuery("SELECT PropertyID, PropertyStreetAddress, ZipCode, AskingPrice, NumberOfBedrooms, NumberOfBathrooms, TotalSquareFeet, YearBuilt FROM HSHProperty WHERE NumberOfBedrooms LIKE ? AND NumberOfBathrooms LIKE ? AND TotalSquareFeet >= ? ORDER BY PropertyID")
+            DB.ExecuteQuery("SELECT PropertyID, PropertyStreetAddress, ZipCode, AskingPrice, NumberOfBedrooms, NumberOfBathrooms, TotalSquareFeet, YearBuilt FROM HSHProperty WHERE NumberOfBedrooms LIKE ? AND NumberOfBathrooms LIKE ? AND TotalSquareFeet >= ? AND Sold = 'n' ORDER BY PropertyID")
         Else
             DB.AddParam("@AskingPrice", askingPriceMaskedTextBox.Text & "%")
-        
-        DB.ExecuteQuery("SELECT PropertyID, PropertyStreetAddress, ZipCode, AskingPrice, NumberOfBedrooms, NumberOfBathrooms, TotalSquareFeet, YearBuilt FROM HSHProperty WHERE NumberOfBedrooms LIKE ? AND NumberOfBathrooms LIKE ? AND TotalSquareFeet >= ?  AND AskingPrice <= ? ORDER BY PropertyID")
-            End If
+
+            DB.ExecuteQuery("SELECT PropertyID, PropertyStreetAddress, ZipCode, AskingPrice, NumberOfBedrooms, NumberOfBathrooms, TotalSquareFeet, YearBuilt FROM HSHProperty WHERE NumberOfBedrooms LIKE ? AND NumberOfBathrooms LIKE ? AND TotalSquareFeet >= ?  AND AskingPrice <= ? AND Sold = 'n' ORDER BY PropertyID")
+        End If
 
         If DB.Exception <> String.Empty Then
             MessageBox.Show(DB.Exception)
@@ -45,12 +48,142 @@
         SquareFeetMaskedTextBox.Text = ""
         askingPriceMaskedTextBox.Text = ""
 
-        DB.ExecuteQuery("SELECT PropertyID, PropertyStreetAddress, ZipCode, AskingPrice, NumberOfBedrooms, NumberOfBathrooms, TotalSquareFeet, YearBuilt FROM HSHProperty ORDER BY PropertyID")
+        DB.ExecuteQuery("SELECT PropertyID, PropertyStreetAddress, ZipCode, AskingPrice, NumberOfBedrooms, NumberOfBathrooms, TotalSquareFeet, YearBuilt FROM HSHProperty WHERE Sold = 'n' ORDER BY PropertyID")
         If DB.Exception <> String.Empty Then
             MessageBox.Show(DB.Exception)
             Exit Sub
         End If
 
         HSHPropertyDataGridView.DataSource = DB.DBDataTable
+    End Sub
+
+    Private Sub HSHPropertyDataGridView_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles HSHPropertyDataGridView.CellClick
+
+        'select the property to sell by clicking on the row in the grid
+        Dim PropertyID As Object
+        If e.RowIndex > -1 Then
+            PropertyID = HSHPropertyDataGridView.Rows(e.RowIndex).Cells(0).Value()
+        End If
+
+        PropertyIDTextBox.Text = PropertyID.ToString
+
+        DB.ExecuteQuery("SELECT MAX(SaleId) as MaxSaleID FROM HSHClosingSale")
+
+        If Not String.IsNullOrEmpty(DB.Exception) Then
+            MessageBox.Show(DB.Exception)
+            Exit Sub
+        End If
+
+        SaleIDTextBox.Text = DB.DBDataTable.Rows(0).Item(0) + 1
+
+        DateSoldMaskedTextBox.Text = String.Format("{0:yyyy/MM/dd}", DateTime.Now)
+
+        'enable the sales buttons
+        PurchaseButton.Enabled = True
+        ClearPurchaseButton.Enabled = True
+
+    End Sub
+
+    Private Sub PurchaseButton_Click(sender As Object, e As EventArgs) Handles PurchaseButton.Click
+
+        Dim NewPropertiesSoldInteger, NewTotalTalesInteger As Integer
+
+        'validate entries
+        'still need to figure out how to ensure that customer and real estate values are validated against their respective data bases
+        If CustomerIDMaskedTextBox.Text = "" Then
+            MessageBox.Show("Please enter the Customer ID", "Missing Sales Information", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            CustomerIDMaskedTextBox.Focus()
+        ElseIf RealEstateAgentIDMaskedTextBox.Text = "" Then
+            MessageBox.Show("Please enter the Real Estate Agent ID", "Missing Sales Information", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            RealEstateAgentIDMaskedTextBox.Focus()
+        ElseIf SalePriceMaskedTextBox.Text = "" Then
+            MessageBox.Show("Please enter the Sale Price", "Missing Sales Information", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            SalePriceMaskedTextBox.Focus()
+        Else
+            DB.AddParam("@SaleID", SaleIDTextBox.Text)
+            DB.AddParam("@PropertyID", PropertyIDTextBox.Text)
+            DB.AddParam("@RealEstateAgentID", RealEstateAgentIDMaskedTextBox.Text)
+            DB.AddParam("@CustomerID", CustomerIDMaskedTextBox.Text)
+            DB.AddParam("@DateSold", DateSoldMaskedTextBox.Text)
+            DB.AddParam("@SalePrice", SalePriceMaskedTextBox.Text)
+
+            'create hshclosing insert
+            DB.ExecuteQuery("INSERT INTO HSHClosingSale VALUES (?, ?, ?, ?, ?, ?)")
+
+            If Not String.IsNullOrEmpty(DB.Exception) Then
+                MessageBox.Show(DB.Exception)
+                Exit Sub
+            End If
+
+            'update hshproperty to reflect sold status
+            DB.AddParam("@propertyid", PropertyIDTextBox.Text)
+            DB.ExecuteQuery("UPDATE HSHProperty SET Sold = 'y' WHERE PropertyID = ?")
+
+
+            If Not String.IsNullOrEmpty(DB.Exception) Then
+                MessageBox.Show(DB.Exception)
+                Exit Sub
+            End If
+
+            'update hshrealestateagent to relect sold status
+            DB.AddParam("@RealEstateAgentID", RealEstateAgentIDMaskedTextBox.Text)
+            DB.ExecuteQuery("SELECT RealEstateSoldtoDate, TotalSalestoDate FROM HSHRealEstateAgent WHERE RealEstateAgentID = ?")
+
+            If Not String.IsNullOrEmpty(DB.Exception) Then
+                MessageBox.Show(DB.Exception)
+                Exit Sub
+            End If
+
+            NewPropertiesSoldInteger = DB.DBDataTable.Rows(0).Item(0) + 1
+            NewTotalTalesInteger = DB.DBDataTable.Rows(0).Item(1) + SalePriceMaskedTextBox.Text
+
+            DB.AddParam("@RealEstateSoldtoDate", NewPropertiesSoldInteger)
+            DB.AddParam("@TotalSalestoDate", NewTotalTalesInteger)
+            DB.AddParam("@RealEstateAgentID", RealEstateAgentIDMaskedTextBox.Text)
+
+            DB.ExecuteQuery("UPDATE HSHRealEstateAgent SET RealEstateSoldtoDate = ?, TotalSalestoDate = ? WHERE RealEstateAgentID = ?")
+
+            If Not String.IsNullOrEmpty(DB.Exception) Then
+                MessageBox.Show(DB.Exception)
+                Exit Sub
+            End If
+
+            'clear sales boxes and disable sales buttons
+            SaleIDTextBox.Clear()
+            PropertyIDTextBox.Clear()
+            RealEstateAgentIDMaskedTextBox.Clear()
+            CustomerIDMaskedTextBox.Clear()
+            DateSoldMaskedTextBox.Clear()
+            SalePriceMaskedTextBox.Clear()
+
+            PurchaseButton.Enabled = False
+            ClearPurchaseButton.Enabled = False
+
+            'reload table
+            DB.ExecuteQuery("SELECT PropertyID, PropertyStreetAddress, ZipCode, AskingPrice, NumberOfBedrooms, NumberOfBathrooms, TotalSquareFeet, YearBuilt FROM HSHProperty WHERE Sold = 'n' ORDER BY PropertyID")
+            If DB.Exception <> String.Empty Then
+                MessageBox.Show(DB.Exception)
+                Exit Sub
+            End If
+
+            HSHPropertyDataGridView.DataSource = DB.DBDataTable
+        End If
+
+
+    End Sub
+
+    Private Sub ClearPurchaseButton_Click(sender As Object, e As EventArgs) Handles ClearPurchaseButton.Click
+
+        'clear the purchase area and disable the purchase buttons
+        SaleIDTextBox.Clear()
+        PropertyIDTextBox.Clear()
+        RealEstateAgentIDMaskedTextBox.Clear()
+        CustomerIDMaskedTextBox.Clear()
+        DateSoldMaskedTextBox.Clear()
+        SalePriceMaskedTextBox.Clear()
+
+        PurchaseButton.Enabled = False
+        ClearPurchaseButton.Enabled = False
+
     End Sub
 End Class
